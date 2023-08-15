5ef1302d59cf1e62de74394f6631888ef8aba4a0
Jack-shah <116387506+Jack-shah@users.noreply.github.com>
2023-08-15 13:02:18 +0530

    Delete Backend.tf

--- Backend.tf
+++
@@ -1,9 +0,0 @@
-# this backend.tf file is used for setting remote setup for storing terraform.tf file
-terraform {
-  backend "s3" {
-    bucket = "abdul-tfstat"
-    key    = "terraform-tfstate" #folder inside bucket where you will store your terraform.tfstate file
-    region = "us-east-1"
-    #here dynamodb-table also can be added for locking feature of the terraform.tfstate file but i have not added because i am worried for cost
-  }
-}
